9404382a74c24707de7cc31f3da35dc6c9abd1f7
Keanu Williams <keanucwilliams@icloud.com>
2022-05-02 15:57:29 -1000

    Update styling and fixed bug where page was overflowing

--- src/app/App.js
+++ src/app/App.js
@@ -5,14 +5,14 @@ import '../styling/App.css';
 
 function App() {
   return (
-    <>
+    <div id='wrapper'>
       <div id='header-bg'></div>
       <div id='overlay'></div>
       <Header />
       <AnchorLink text='View my Projects' isDown sectionId='projects' />
       <div id='projects-bg'></div>
       <Projects />
-    </>
+    </div>
   );
 }
 
